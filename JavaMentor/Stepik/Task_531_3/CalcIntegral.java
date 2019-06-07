9db9041b68a8d567335d4181e0a008e281dcc68d
mysmurov <mysmurov@gmail.com>
2019-06-07 12:37:06 +0300

    задачи со stepik.org 5.3.1

--- JavaMentor/Stepik/Task_531_3/CalcIntegral.java
+++ JavaMentor/Stepik/Task_531_3/CalcIntegral.java
@@ -1,4 +1,4 @@
-package JavaMentor.Stepik;
+package JavaMentor.Stepik.Task_531_3;
 
 import java.util.function.DoubleUnaryOperator;
 
@@ -8,20 +8,22 @@ public class CalcIntegral {
 
     public static void main(String[] args) {
         System.out.println(integrate(x -> 1, 0, 10));//10.0
+        System.out.println(integrate(x -> x + 2, 0, 10));//70.0
+        System.out.println(integrate( x -> Math.sin(x) / x , 1, 5));//0.603848
     }
 
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
         double s = 0;
         double x = 1e-6;
-        double n = (b - a) / x;
 
-//        while (a < b){
-//            s += f.applyAsDouble(a += x)*x;
+        while (a < b){
+            s += f.applyAsDouble(a += x)*x;
 
+            // вариант через  for
 
-        for (; a < b; a += x) {
-            s += f.applyAsDouble(a) * x;
+//        for (; a < b; a += x) {
+//            s += f.applyAsDouble(a) * x;
         }
-        return (int)s;
+        return s;
     }
 }
